bb78d334e439108f2c89f226e9529b672d326405
gercek1 <olcanchano@gmail.com>
2024-07-28 15:57:31 -0500

    probando la funcionalidad Feature Branching

--- src/index.js
+++ src/index.js
@@ -1,6 +1,40 @@
 import './styles/index.scss';
-const saludar = () => {
-    console.log("Hola, mundo!");
-}
+// function listaProductos(producto,contenedor){
+//     contenedor.innerHTML = "";
+//     producto.forEach(productos => {
+//         const div = document.createElement("div");
+//         const h2 = document.createElement("h2");
+//         const figura = document.createElement("figure");
+//         const imagen = document.createElement("img");
 
-saludar();
+//         imagen.setAttribute("src", productos.imagen);
+//         h2.textContent = productos.nombre;
+
+//         figura.appendChild(imagen);
+//         div.appendChild(h2);
+//         div.appendChild(figura);
+//         contenedor.appendChild(div);
+//     });
+// }
+
+// const productos = [
+//     {
+//         id: 1,
+//         nombre: "Producto 1",
+//         imagen: "https://yt3.ggpht.com/a/AATXAJzPD0XBaRxiZzDMzWeev6tLfvfz_m70bZoZ2Qya=s900-c-k-c0xffffffff-no-rj-mo"
+//     },
+//     {
+//         id: 2,
+//         nombre: "Producto 2",
+//         imagen: "https://xsurf.es/wp-content/uploads/2021/02/Waves.jpg",
+//     },
+//     {
+//         id: 3,
+//         nombre: "Producto 3",
+//         imagen: "https://xsurf.es/wp-content/uploads/2021/02/Waves.jpg",
+//     }
+// ]
+
+// const contenedor = document.getElementById("prueba");
+
+// listaProductos(productos,contenedor);
