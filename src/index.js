fc611b450e13e522fca9ceeaaf38a046ad86cdb6
gercek1 <olcanchano@gmail.com>
2024-08-04 22:15:47 -0500

    se completo la funcion actualizarMain

--- src/index.js
+++ src/index.js
@@ -48,55 +48,61 @@ const obtenerUrlPokemons = async (listaPokemon) => {
 
 
 function pintarPokemonesMain(pokemons, contenedor) {
-    const lista = document.createElement("ul");
     pokemons.forEach((element, index) => {
         if (index === 0) {
-            const card = document.createElement("article");
-            const card2 = document.createElement("article");
-            const figure = document.createElement("figure");
-            const imagen = document.createElement("img");
-            const h2 = document.createElement("h2");
-            const id = document.createElement("li");
-            const peso = document.createElement("li");
-            const altura = document.createElement("li");
-            const tipo = document.createElement("li");
-            const nivel = document.createElement("li");
-            const habilidad = document.createElement("li");
-
-            h2.textContent = element.nombre;
-            imagen.setAttribute("src", element.imagen)
-            card.classList.add("card");
-
-            card.appendChild(h2);
-            figure.appendChild(imagen);
-            card.appendChild(figure);
-            contenedor.appendChild(card);
-            card2.appendChild(lista);
-            id.textContent = `ID: ${element.id}`;
-            nivel.textContent = `Nivel: ${element.nivel}`;
-            tipo.textContent = `Tipo: ${element.tipo}`;
-            habilidad.textContent = `Habilidades: ${element.habilidades}`;
-            peso.textContent = `Peso: ${element.peso}kg`;
-            altura.textContent = `Altura: ${element.altura}m`;
-            lista.appendChild(id);
-            lista.appendChild(nivel);
-            lista.appendChild(tipo);
-            lista.appendChild(peso);
-            lista.appendChild(habilidad);
-            lista.appendChild(altura);
-            contenedor.appendChild(card2);
+            actualizarMain(element, contenedor)
         }
     });
 
 }
 
-function pintarPokemonesFooter(pokemons, contenedor) {
+function actualizarMain(element, contenedor) {
+    contenedor.innerHTML = "";
+
+
+    const card = document.createElement("article");
+    const card2 = document.createElement("article");
+    const figure = document.createElement("figure");
+    const imagen = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const lista = document.createElement("ul");
+    const id = document.createElement("li");
+    const peso = document.createElement("li");
+    const altura = document.createElement("li");
+    const tipo = document.createElement("li");
+    const nivel = document.createElement("li");
+    const habilidad = document.createElement("li");
+
+    h2.textContent = element.nombre;
+    imagen.setAttribute("src", element.imagen)
+    card.classList.add("card");
+
+    card.appendChild(h2);
+    figure.appendChild(imagen);
+    card.appendChild(figure);
+    contenedor.appendChild(card);
+    card2.appendChild(lista);
+    id.textContent = `ID: ${element.id}`;
+    nivel.textContent = `Nivel: ${element.nivel}`;
+    tipo.textContent = `Tipo: ${element.tipo}`;
+    habilidad.textContent = `Habilidades: ${element.habilidades}`;
+    peso.textContent = `Peso: ${element.peso}kg`;
+    altura.textContent = `Altura: ${element.altura}m`;
+    lista.appendChild(id);
+    lista.appendChild(nivel);
+    lista.appendChild(tipo);
+    lista.appendChild(peso);
+    lista.appendChild(habilidad);
+    lista.appendChild(altura);
+    contenedor.appendChild(card2);
+}
+
+function pintarPokemonesFooter(pokemons, footer) {
     let count = 0;
     let maxPokemon = 4;
     pokemons.forEach((element) => {
         if (count < maxPokemon) {
             const card = document.createElement("article");
-            const card2 = document.createElement("article");
             const figure = document.createElement("figure");
             const imagen = document.createElement("img");
             const h2 = document.createElement("h2");
@@ -108,7 +114,11 @@ function pintarPokemonesFooter(pokemons, contenedor) {
             card.appendChild(h2);
             figure.appendChild(imagen);
             card.appendChild(figure);
-            contenedor.appendChild(card);
+            footer.appendChild(card);
+
+            card.addEventListener("click", () => {
+                actualizarMain(element, contenedor);
+            });
 
             count++;
         } else {
@@ -117,6 +127,7 @@ function pintarPokemonesFooter(pokemons, contenedor) {
     });
 
 }
+
 document.addEventListener("DOMContentLoaded", async () => {
     const pokemon = await getPokemons(URL_API);
     const obternerPokemones = await obtenerUrlPokemons(pokemon);
@@ -124,36 +135,4 @@ document.addEventListener("DOMContentLoaded", async () => {
     pintarPokemonesMain(obternerPokemones, contenedor);
     pintarPokemonesFooter(obternerPokemones, footter);
 })
-//         imagen.setAttribute("src", productos.imagen);
-//         h2.textContent = productos.nombre;
-
-//         figura.appendChild(imagen);
-//         div.appendChild(h2);
-//         div.appendChild(figura);
-//         contenedor.appendChild(div);
-//     });
-// }
-
-// const productos = [
-//     {
-//         id: 1,
-//         nombre: "Producto 1",
-//         imagen: "https://yt3.ggpht.com/a/AATXAJzPD0XBaRxiZzDMzWeev6tLfvfz_m70bZoZ2Qya=s900-c-k-c0xffffffff-no-rj-mo"
-//     },
-//     {
-//         id: 2,
-//         nombre: "Producto 2",
-//         imagen: "https://xsurf.es/wp-content/uploads/2021/02/Waves.jpg",
-//     },
-//     {
-//         id: 3,
-//         nombre: "Producto 3",
-//         imagen: "https://xsurf.es/wp-content/uploads/2021/02/Waves.jpg",
-//     }
-// ]
-
-// const contenedor = document.getElementById("prueba");
-
-// listaProductos(productos,contenedor);
-
 
